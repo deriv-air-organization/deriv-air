3b18997bab2e667ffc4230edbc1081f0033ddbdb
Farzin Mirzaie <farzin@firstsource.tech>
2022-10-11 09:20:09 +0330

    feat: :hammer: update root scripts

--- packages/shared/example/provider/index.tsx
+++ packages/shared/example/provider/index.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 import { Dripsy } from './dripsy'
 import { NavigationProvider } from './navigation'
 
-export function Provider({ children }: { children: React.ReactNode }) {
+export function Provider({
+  children,
+  withNavigation = false,
+}: {
+  children: React.ReactNode
+  withNavigation?: boolean
+}) {
+  if (!withNavigation) return <Dripsy>{children}</Dripsy>
+
   return (
     <NavigationProvider>
       <Dripsy>{children}</Dripsy>
